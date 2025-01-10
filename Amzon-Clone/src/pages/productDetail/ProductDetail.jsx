da4c8ad9f136d99f077b8739ef3652f7561cf495
gechjs <gizachew980@gmail.com>
2025-01-10 19:29:42 +0300

    improve error handling

--- Amzon-Clone/src/pages/productDetail/ProductDetail.jsx
+++ Amzon-Clone/src/pages/productDetail/ProductDetail.jsx
@@ -5,24 +5,23 @@ import { useParams } from "react-router-dom";
 import instance from "../../utils/axios.js";
 import ProductCard from "../../components/Product/ProductCard.jsx";
 import Loader from "../../components/loader/Loader.jsx";
-import ErrorBoundary from "../../components/ErrorBoundary/ErrorBoundary"; // Import ErrorBoundary
+import ErrorBoundary from "../../components/ErrorBoundary/ErrorBoundary";
 
 const ProductDetail = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null); 
   const { productId } = useParams();
-  const [isLoading, setLoading] = useState(false);
-  const [error, setError] = useState(null); // State to handle errors
+  const [isLoading, setLoading] = useState(true); 
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    setLoading(true);
     const fetchData = async () => {
       try {
         const response = await instance(`products/${productId}`);
-        setData(response.data);
+        setData(response.data); 
       } catch (err) {
-        setError(err); 
+        setError(err);
       } finally {
-        setLoading(false);
+        setLoading(false); 
       }
     };
     fetchData();
@@ -30,13 +29,15 @@ const ProductDetail = () => {
 
   return (
     <LayOut>
-      <ErrorBoundary> {/* Wrap the component with ErrorBoundary */}
+      <ErrorBoundary>
         {isLoading ? (
           <Loader />
         ) : error ? (
-          <div>An error occurred: {error.message}</div> 
-        ) : (
+          <div>An error occurred: {error.message}</div>
+        ) : data ? ( 
           <ProductCard renderDesc={true} flex={true} product={data} />
+        ) : (
+          <div>Product not found.</div> 
         )}
       </ErrorBoundary>
     </LayOut>
